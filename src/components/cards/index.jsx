33bff57ec7c44533e641961bbe5a26672f799635
AzizJakbaraliyev <azizjakbaraliyev454@gamil.com>
2024-12-19 16:35:17 +0500

    Responsive boldi

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -4,9 +4,13 @@ import { HeartOutlined } from "@ant-design/icons";
 function Card(data) {
   return (
     // <section className="container flex items-start justify-between">
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 max-[435px]:text-[0.8em] max-[370px]:text-[0.7em] max-[334px]:text-[1em]">
       <div className="w-[100%]">
-        <img className="w-full h-[280px] rounded-md" src={data.img} alt="" />
+        <img
+          className="w-full h-[280px] rounded-md max-[435px]:h-[220px]  max-[334px]:h-[300px]"
+          src={data.img}
+          alt=""
+        />
       </div>
       <div className="flex flex-col gap-5">
         <div>
